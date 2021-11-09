5680ad945168adfe8022317e69f9e072dfbfa4ab
Alex <87618474+vattgern@users.noreply.github.com>
2021-11-09 10:09:32 +0400

    Add files via upload
    начинаю ajax

---
+++ assets/js/addbook.js
@@ -0,0 +1,8 @@
+const name_b = document.querySelector("#name-book");
+const name_a = document.querySelector("#name-author");
+const genre_b = document.querySelector("#genre-book");
+const description = document.querySelector("#description-book");
+const year_b = document.querySelector("#year-book");
+
+const url = "add_book.php";
+const params = ""
