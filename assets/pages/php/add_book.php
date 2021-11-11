b462b81decd16c9df90dd84412279151a0af3908
vattgern <sashawot060@gmail.com>
2021-11-11 19:14:41 +0400

    ITS WORKS I DID IT FINALLY

--- assets/pages/php/add_book.php
+++ assets/pages/php/add_book.php
@@ -21,3 +21,15 @@ $data = [
 ];
 $database->query("INSERT INTO `books` (`id_book`, `title_book`, `genre_book`, `desc_book`, `img_book`, `year_book`) VALUES (NULL, '{$data['name_book']}', '{$data['genre_book']}', '{$data['desc']}', '' , '{$data['year_book']}')");
 $database->query("INSERT INTO `authors` (`id_author`, `name_author`) VALUES (NULL , '{$data['name_author']}')");
+
+$answerOne = $database->query("SELECT id_author FROM `authors` WHERE `name_author` = '{$data['name_author']}'");
+$id_author = $answerOne->fetch(PDO::FETCH_ASSOC);
+
+$answerTwo = $database->query("SELECT id_book FROM `books` WHERE `title_book` = '{$data['name_book']}'");
+$id_book = $answerTwo->fetch(PDO::FETCH_ASSOC);
+
+$id_book['id_book'] = (int) $id_book['id_book'];
+$id_author['id_author'] = (int) $id_author['id_author'];
+
+//$database->query("INSERT INTO `usersbooks` (`id_b`, `id_a`) VALUES ('{$id_book['id_book']}', '{$id_author['id_author']}'");
+$database->query("INSERT INTO `usersbooks` (`id_b`, `id_a`) VALUES ('{$id_book['id_book']}', '{$id_author['id_author']}')");
